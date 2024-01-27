3dab368874c11db5d2a443f214475cc6629e180a
pratirath06 <119922757+pratirath06@users.noreply.github.com>
2024-01-28 00:42:35 +0530

    Create float.c

---
+++ DataTypes/float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float i = 9.0f;
+    float j = 2.8f;
+    //"%f" is used for float
+    printf("%f",j);
+    //Output:2.800000
+    printf("\n%f",i);
+    //Output:9.000000
+    return 0;
+}
